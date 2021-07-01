24f4f8966d7ca465d488bdf515d7912f3dea34b1
pomaskin@rambler.ru <pomaskin@rambler.ru>
2021-07-01 23:51:21 +0500

    fixed bugs

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -7,20 +7,21 @@ void main()
 {
 	setlocale(LC_ALL, "Russian");
 	const int SIZE = 5;
-	int arr[SIZE] = {};
+	int arr[SIZE];
 	double summ = 0;
-	double summ1 = 0;
-	int max = arr[0], min = arr[0];
-
+	int max, min;
 	cout << "Введите значения элементов: ";
 	for (int i = 0; i < SIZE; i++)
 	{
 		cin >> arr[i];
 		cout << arr[i] << tab;
 		summ += arr[i];
-		summ1 = summ / SIZE;
-		if (arr[i] > max) max = arr[i];
+	}
+	min = max = arr[0];
+	for (int i = 0; i < SIZE; i++)
+	{
 		if (arr[i] < min) min = arr[i];
+		if (arr[i] > max) max = arr[i];
 	}
 	cout << endl;
 	for (int i = SIZE - 1; i >= 0; i--)
@@ -29,7 +30,7 @@ void main()
 	}
 	cout << endl;
 	cout << "Сумма всех элементов в массиве: " << summ << endl;
-	cout << "Cреднее арифметическое значение элементов в массиве: " << summ1 << endl;
+	cout << "Cреднее арифметическое значение элементов в массиве: " << summ/SIZE << endl;
 	cout << "Максимальное значение в массиве: " << max << endl;
-	cout << "Минемальное значение в массиве: " << min << endl;
+	cout << "Минимальное значение в массиве: " << min << endl;
 }
